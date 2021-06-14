330e3d7901e0b77eca9aaf6d1f288eadc12e9ad9
Saransh Rakshak <40779724+saranshrakshak@users.noreply.github.com>
2021-06-13 20:09:34 -0700

    opiate analysis for both python and jupitenotebook

---
+++ opiate_analysis.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# In[ ]:
+
+
+
+
+
+# Dataset provided by the Center for Medicare and Medicaid Services, which can be accessed here: 
+# https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/OpioidMap
+# 
+# 
+
+# In[6]:
+
+
+op_data = pd.read_csv("op_data.csv")
+op_data
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
